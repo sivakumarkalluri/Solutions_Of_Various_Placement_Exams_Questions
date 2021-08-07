8b133083cfc96a9b32a41e5240d9687874273514
sivakumarkalluri <sivakumarreddykalluri@gmail.com>
2021-08-07 10:27:48 +0530

    SpecialCharacteOrNot.py 07-8

---
+++ SpecialCharacteOrNot.py
@@ -0,0 +1,5 @@
+char=input("enter the string value")
+if 'a'<=char<='z' or 'A'<=char<='Z' or '0'<=char<='9':
+    print(char,"is not a special character")
+else:
+    print(char,'is a special character')
