2d87d1b00cc3d274a9543c65e708b481d98207c6
sivakumarkalluri <sivakumarreddykalluri@gmail.com>
2021-08-07 10:13:41 +0530

    MultipleOrNot.py added

---
+++ MultipleOrNot.py
@@ -0,0 +1,6 @@
+Value=int(input("Enter the number to check the multiple : "))
+multiple=int(input("Enter the multiple : "))
+if Value%multiple==0:
+    print(Value,"is multiple of",multiple)
+else:
+     print(Value,"is not multiple of",multiple)
