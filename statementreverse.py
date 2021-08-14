bcbf403fb297ccf7d5c80b36f9557565094e5d15
sivakumarkalluri <sivakumarreddykalluri@gmail.com>
2021-08-14 10:52:17 +0530

    statement reverse 14-08-21

--- statementreverse.py
+++ statementreverse.py
@@ -1,17 +1,30 @@
 statement="hi hello how are you"
+# >>>>>>>>>>>>>>>splitting the satement in reverse<<<<<<<<<<<<<<<<<<
+
 # statement=statement.split()
 # output=" ".join(statement[::-1])
 # print(output)
-l=[""]
-output=""
-for i in statement:
-    if i==" ":
-        l.append('')
-    else:
-        l[-1]+=i
-for i in range(1,len(l)+1):
-    output+=l[-i]
 
-    if i!=len(l):
-        output+=" "
-print(output)
+# >>>>>>>>>>>>>>>splitting the satement in reverse WITHOUT SPLIT AND JOIN FUNCTIONS<<<<<<<<<<<<<<<<<<
+
+# l=[""]
+# output=""
+# for i in statement:
+#     if i==" ":
+#         l.append('')
+#     else:
+#         l[-1]+=i
+# for i in range(1,len(l)+1):
+#     output+=l[-i]
+
+#     if i!=len(l):
+#         output+=" "
+# print(output)
+
+# >>>>>>>>>>>>>>>splitting the every word in satement in reverse<<<<<<<<<<<<<<<<<<
+
+statement=statement.split()
+l=[]
+for i in statement:
+    l.append(i[::-1])
+print(' '.join(l))
