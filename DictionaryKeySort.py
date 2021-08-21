83d8a5cd24a5464ffc6a2b2c81c7e8ca8ec8aba6
sivakumarkalluri <sivakumarreddykalluri@gmail.com>
2021-08-21 09:47:12 +0530

    sorting the dictionary keys 21-08-21

---
+++ DictionaryKeySort.py
@@ -0,0 +1,9 @@
+dictionary={'hai':5,'program':9,'pink':67,'collection':4,'by':8,'class':6}
+res=[]
+for i in dictionary:
+    res.append((i,dictionary[i]))
+for j in range(len(res)-1):
+    for k in range(j+1,len(res)):
+        if res[j][0]>res[k][0]:
+            res[j],res[k]=res[k],res[j]
+print(dict(res))
